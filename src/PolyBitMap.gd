1c97bac2acd946a62c8ec4f5011617c230938c3e
David Zajicek <david.a.zajicek@outlook.com>
2023-02-19 08:17:42 +1100

    setget changed in godot 4

--- src/PolyBitMap.gd
+++ src/PolyBitMap.gd
@@ -1,6 +1,8 @@
 class_name PolyBitMap
 extends BitMap
 
+var inner_walls: PackedVector2Array : get = get_all_inner_walls
+
 func get_all_inner_walls():
 	var points: PackedVector2Array
 	for x in self.get_size().x:
