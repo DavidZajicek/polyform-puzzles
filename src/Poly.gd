fb5f04dcfc1458efaedba64819635992fa7addb7
David Zajicek <david.a.zajicek@outlook.com>
2023-02-22 06:19:47 +1100

    Added exponential score system to poly

--- src/Poly.gd
+++ src/Poly.gd
@@ -1,3 +1,4 @@
 class_name Poly
 extends Area2D
 
+var score: int = 1
--- src/Polyomino.gd
+++ src/Polyomino.gd
@@ -16,51 +16,20 @@ var mouse_over: bool = false
 func _ready() -> void:
 	
 	randomize()
-#	var points: PackedVector2Array = generate_shape()
-#	polyforms_resource.generate_shape(1)
-#	polyforms_resource.generate_shape(2)
-#	polyforms_resource.generate_shape(3)
-#	polyforms_resource.generate_shape(4) #19/5
-#	polyforms_resource.generate_shape(5) #63/12
-#	polyforms_resource.generate_shape(6) #216/35 (all shapes) 00:03
-#	polyforms_resource.generate_shape(7) #760/108 (all shapes) 0:27
-#	polyforms_resource.generate_shape(8) #2725/369 (all shapes) 4:47
-#	polyforms_resource.generate_shape(9) #9910/1285 (all shapes) 01:40:34
-#	polyforms_resource.generate_shape(10) #36446/4655 5 hours
 	generate_shape(size)
 	connect_with_poly_children()
 	
 
-
-#func _physics_process(delta: float) -> void:
-#	if dragging:
-#		var mouse_pos: Vector2 = InputEventScreenDrag.position
-#		position = mouse_pos + offset
-
-#func _input(event: InputEvent) -> void:
-#
-##	if Input.is_action_just_pressed("left_mouse") and mouse_over:
-##		if Input.is_action_just_pressed("left_mouse"):
-##			original_position = position
-##			offset = position - get_global_mouse_position()
-##			dragging = true
-##			get_tree().get_root().set_input_as_handled()
-##			z_index = 1
-#	if Input.is_action_just_released("left_mouse") and dragging:
-#		position = snapped(position, Vector2(32,32))
-#		for child in overlap_areas:
-#			if child.has_overlapping_areas():
-#				position = original_position
-#		dragging = false
-#		z_index = 0
-#
-
 func generate_shape(size: int) -> void:
 	var size_poly = polyforms_resource.polyominoes[size]
+	var score: int = 0
 	for vector in size_poly[size_poly.keys()[randi() % size_poly.size()]].get_all_inner_walls():
 		var new_poly = poly.instantiate()
 		add_child(new_poly)
 		new_poly.position = vector * Globals.tile_size
+		score += 1
+	for child in get_children():
+		child.score = score * score
 
 
 func connect_with_poly_children() -> void:
